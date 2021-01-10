d445f48a12f61999536d0f60b6a27a4fa8687973
FannyBesson <besson.fa@gmail.com>
2021-01-10 15:39:32 +0100

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO BUIDLING(id, name) VALUES(-10, 'Building1');
+INSERT INTO BUILDING(id, name) VALUES(-10, 'Building1');
 
 INSERT INTO ROOM(id, name, floor, current_temperature, target_temperature) VALUES(-10, 'Room1', 1, 22.3, 20.0);
 INSERT INTO ROOM(id, name, floor) VALUES(-9, 'Room2', 1);
@@ -10,5 +10,6 @@ INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-10, 'CLOSED', 'Win
 INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-9, 'CLOSED', 'Window 2', -10);
 INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-8, 'OPEN', 'Window 1', -9);
 INSERT INTO RWINDOW(id, window_status, name, room_id) VALUES(-7, 'CLOSED', 'Window 2', -9);
+
 
 
